56760ff35c18d2bef647a549536347b336928697
kanishka-sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-24 14:17:27 +0530

    Log In And Generating JWT

--- Gemfile
+++ Gemfile
@@ -20,14 +20,14 @@ gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
+gem 'bcrypt', '~> 3.1.7'
+gem 'jwt'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-
+gem 'solargraph'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,24 @@
+class AuthenticationController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def login
+        @user=User.find_by_email(params[:email])
+        if @user&.authenticate(params[:password])
+            token = JsonWebToken.encode(user_id: @user.id)
+            time=Time.now + 2.hours.to_i
+            render json: {
+                token: token, 
+                exp: time.strftime("%d-%m-%Y %H:%M"),
+                username: @user.name}, status: :ok
+        else
+            render json: { error: 'unauthorized' }, status: :unauthorized 
+        end
+
+    end
+
+    private
+
+    def user_params
+        params.permit(:email,:password)
+    end
+end
